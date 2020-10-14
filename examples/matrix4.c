b87e001327231d78dbde10c1e589832ebab8774f
火星大王 <lijunyandeyouxiang@163.com>
2020-10-14 21:52:25 +0800

    在行列式求值前显示行列式

--- examples/matrix4.c
+++ examples/matrix4.c
@@ -10,6 +10,7 @@ int main()
 		m1=jml_matrix_set(m1,0,0,1);	m1=jml_matrix_set(m1,0,1,1);	m1=jml_matrix_set(m1,0,2,1);
 		m1=jml_matrix_set(m1,1,0,1);	m1=jml_matrix_set(m1,1,1,2);	m1=jml_matrix_set(m1,1,2,3);
 		m1=jml_matrix_set(m1,2,0,1);	m1=jml_matrix_set(m1,2,1,3);	m1=jml_matrix_set(m1,2,2,6);
+		jml_matrix_view(m1);
 		pint(jml_matrix_determinant(m1));pn();
 		m1=jml_matrix_free(m1);
 	}
@@ -19,12 +20,14 @@ int main()
 		m1=jml_matrix_set(m1,1,0,0);	m1=jml_matrix_set(m1,1,1,2);	m1=jml_matrix_set(m1,1,2,-4);	m1=jml_matrix_set(m1,1,3,-2);
 		m1=jml_matrix_set(m1,2,0,0);	m1=jml_matrix_set(m1,2,1,-1);	m1=jml_matrix_set(m1,2,2,1);	m1=jml_matrix_set(m1,2,3,1);
 		m1=jml_matrix_set(m1,3,0,0);	m1=jml_matrix_set(m1,3,1,2);	m1=jml_matrix_set(m1,3,2,-9);	m1=jml_matrix_set(m1,3,3,1);	
+		jml_matrix_view(m1);
 		pint(jml_matrix_determinant(m1));pn();
 		m1=jml_matrix_free(m1);
 	}
 	{
 		jml_matrix* m1=jml_matrix_new(1,1);
 		m1=jml_matrix_set(m1,0,0,-1);
+		jml_matrix_view(m1);
 		pint(jml_matrix_determinant(m1));pn();
 		m1=jml_matrix_free(m1);
 	}
@@ -32,6 +35,7 @@ int main()
 		jml_matrix* m1=jml_matrix_new(2,2);
 		m1=jml_matrix_set(m1,0,0,2);	m1=jml_matrix_set(m1,0,1,3);
 		m1=jml_matrix_set(m1,1,0,3);	m1=jml_matrix_set(m1,1,1,6);
+		jml_matrix_view(m1);
 		pint(jml_matrix_determinant(m1));pn();
 		m1=jml_matrix_free(m1);
 	}		
